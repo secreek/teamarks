8c47e960ce8f724c35221bd9f325820c3e53e21e
Cliff Woo <cliffwoo@gmail.com>
2012-10-21 12:41:09 +0800

    Teamarks Rails Architecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamarks::Application.config.secret_token = 'fbeea26c61c9959ea595ecd7960ed0e38dfa839a7785e9e3de83b2de4fca8898a9b650c95f989e2b6ce4c4d57c998ae300dd33bc4a6631fabc15ff22fc47cc58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Teamarks::Application.config.session_store :cookie_store, key: '_teamarks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Teamarks::Application.config.session_store :active_record_store
