f70cb97ac03a6aed5cbf45a773a030cdb0217373
Cliff Woo <cliffwoo@gmail.com>
2012-10-24 09:31:58 +0800

    add notifier branch

--- src/web/config/environments/production.rb
+++ src/web/config/environments/production.rb
@@ -1,6 +1,11 @@
 Web::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost",
+  port: 25,
+  domain:"smtp.teamarks.com"
+  
+}
   # Code is not reloaded between requests
   config.cache_classes = true
 
