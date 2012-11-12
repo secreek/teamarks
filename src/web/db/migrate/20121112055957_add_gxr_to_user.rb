61c755e599a7d9f166f30499df5878335621ac75
Cliff Woo <cliffwoo@gmail.com>
2012-11-12 14:03:05 +0800

    Add Guo Xinrong to the user table

---
+++ src/web/db/migrate/20121112055957_add_gxr_to_user.rb
@@ -0,0 +1,4 @@
+class AddGxrToUser < ActiveRecord::Migration
+  def change
+  end
+end
--- src/web/db/schema.rb
+++ src/web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025130026) do
+ActiveRecord::Schema.define(:version => 20121112055957) do
 
   create_table "urls", :force => true do |t|
     t.string   "url"
