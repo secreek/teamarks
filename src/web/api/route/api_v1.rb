b2f9d005333c6699a18a23f7fb746da68d093ea0
Void Main <voidmain1313113@gmail.com>
2012-12-07 11:10:36 +0800

    remove all occurrances of /v1/ to namespace, what a brilliant contribution! before ruby application.rb, please re-run bundle install

--- src/web/api/route/api_v1.rb
+++ src/web/api/route/api_v1.rb
@@ -1,188 +1,191 @@
 require 'sinatra'
+require 'sinatra/namespace'
 require 'data_mapper'
 require './model/model.rb'
 require './helper/http_helper.rb'
 require 'json'
+
 # APIs v1
-
-# User APIs
-# for single user
-# get user info
-get '/v1/users/:id' do
-  begin
-    user = User.get!(params[:id])
-    Response.new(200, user).to_json
-  rescue DataMapper::ObjectNotFoundError
-    Response.new(404, 'User not found').to_json
+namespace '/v1' do
+  # User APIs
+  # for single user
+  # get user info
+  get '/users/:id' do
+    begin
+      user = User.get!(params[:id])
+      Response.new(200, user).to_json
+    rescue DataMapper::ObjectNotFoundError
+      Response.new(404, 'User not found').to_json
+    end
   end
-end
 
-# update user info
-put '/v1/users/:id' do
-  begin
-    user = User.get!(params[:id])
-    user.update(User.normalize_params(params))
-    Response.new(200, user).to_json
-  rescue DataMapper::ObjectNotFoundError
-    Response.new(404, 'User not found').to_json
-  rescue DataMapper::SaveFailureError
-    Response.new(500, 'User not inserted').to_json
-  rescue DataMapper::UpdateConflictError
-    Response.new(500, 'Update Conflict').to_json
+  # update user info
+  put '/users/:id' do
+    begin
+      user = User.get!(params[:id])
+      user.update(User.normalize_params(params))
+      Response.new(200, user).to_json
+    rescue DataMapper::ObjectNotFoundError
+      Response.new(404, 'User not found').to_json
+    rescue DataMapper::SaveFailureError
+      Response.new(500, 'User not inserted').to_json
+    rescue DataMapper::UpdateConflictError
+      Response.new(409, 'Update Conflict').to_json
+    end
   end
-end
 
-# delete user
-delete '/v1/users/:id' do
-  begin
-    User.get!(params[:id]).destroy
-    Response.new(200, 'Deleted').to_json
-  rescue DataMapper::ObjectNotFoundError
-    Response.new(404, 'User not found').to_json
+  # delete user
+  delete '/users/:id' do
+    begin
+      User.get!(params[:id]).destroy
+      Response.new(200, 'Deleted').to_json
+    rescue DataMapper::ObjectNotFoundError
+      Response.new(404, 'User not found').to_json
+    end
   end
-end
 
-# test if the unique attributes has been taken
-# arguments:
-#     name - name of the attribute
-#     value - value of the attribute
-# return value:
-#     1 if the attribute is unique and the value has been taken
-#     0 otherwise
-get '/v1/users/attrs/is_unique' do
-  name = params["name"]
-  value = params["value"]
-  return Response.new(200, 0).to_json if !User.is_unique_attribute? name
-  result = (User.count(name => value) == 0)
-  return Response.new(200, result ? 1 : 0).to_json
-end
-
-# insert new user
-post '/v1/users' do
-  begin
-    user = User.new(User.normalize_params(params))
-    user.save
-    Response.new(200, user).to_json
-  rescue DataMapper::SaveFailureError
-    Response.new(500, 'User not inserted').to_json
+  # test if the unique attributes has been taken
+  # arguments:
+  #     name - name of the attribute
+  #     value - value of the attribute
+  # return value:
+  #     1 if the attribute is unique and the value has been taken
+  #     0 otherwise
+  get '/users/attrs/is_unique' do
+    name = params["name"]
+    value = params["value"]
+    return Response.new(200, 0).to_json if !User.is_unique_attribute? name
+    result = (User.count(name => value) == 0)
+    return Response.new(200, result ? 1 : 0).to_json
   end
-end
 
-# get all registered users
-get '/v1/users' do
-  result = []
-  User.all.to_a.each do |user|
-    result << user.to_json_obj
+  # insert new user
+  post '/users' do
+    begin
+      user = User.new(User.normalize_params(params))
+      user.save
+      Response.new(200, user).to_json
+    rescue DataMapper::SaveFailureError
+      Response.new(500, 'User not inserted').to_json
+    end
   end
-  Response.new(200, result).to_json
-end
 
-# Team APIs
-# get a specific team
-get '/v1/teams/:id' do
-end
+  # get all registered users
+  get '/users' do
+    result = []
+    User.all.to_a.each do |user|
+      result << user.to_json_obj
+    end
+    Response.new(200, result).to_json
+  end
 
-# update a team's info
-put '/v1/teams/:id' do
-end
+  # Team APIs
+  # get a specific team
+  get '/teams/:id' do
+  end
 
-# delete a team
-delete '/v1/teams/:id' do
-end
+  # update a team's info
+  put '/teams/:id' do
+  end
 
-# create a new team
-post '/v1/teams' do
-end
+  # delete a team
+  delete '/teams/:id' do
+  end
 
-# get all teams
-get '/v1/teams' do
-end
+  # create a new team
+  post '/teams' do
+  end
 
-# TeamAdmin APIs
-# get all team members for a specfic team
-get '/v1/teams/:id/members' do
-end
+  # get all teams
+  get '/teams' do
+  end
 
-# get all teams a user joined
-get '/v1/users/:id/teams' do
-end
+  # TeamAdmin APIs
+  # get all team members for a specfic team
+  get '/teams/:id/members' do
+  end
 
-# get the role of a user in some team
-get '/v1/teams/:team_id/members/:user_id/role' do
-end
+  # get all teams a user joined
+  get '/users/:id/teams' do
+  end
 
-# get the status of a user in some team
-get '/v1/teams/:team_id/members/:user_id/status' do
-end
+  # get the role of a user in some team
+  get '/teams/:team_id/members/:user_id/role' do
+  end
 
-# insert / update the role of a user in some team
-# arguments:
-#     value - integer, the value of the role [REQUIRED]
-# PUT /v1/teams/1/members/2/role?value=1
-put '/v1/teams/:team_id/members/:user_id/role' do
-end
+  # get the status of a user in some team
+  get '/teams/:team_id/members/:user_id/status' do
+  end
 
-# insert / update the status of a user in some team
-# arguments:
-#     value - integer, the value of the status [REQUIRED]
-# PUT /v1/teams/1/members/2/status?value=2
-put '/v1/teams/:team_id/members/:user_id/status' do
-end
+  # insert / update the role of a user in some team
+  # arguments:
+  #     value - integer, the value of the role [REQUIRED]
+  # PUT /v1/teams/1/members/2/role?value=1
+  put '/teams/:team_id/members/:user_id/role' do
+  end
 
-# remove a member from a team
-delete '/v1/teams/:team_id/members/:user_id' do
-end
+  # insert / update the status of a user in some team
+  # arguments:
+  #     value - integer, the value of the status [REQUIRED]
+  # PUT /v1/teams/1/members/2/status?value=2
+  put '/teams/:team_id/members/:user_id/status' do
+  end
 
-# Marks APIs
-# get the list of marks for a team, grouped by user
-# prepared for paperboy
-# arguments:
-#     later_than - integer [OPTIONAL]
-# GET /v1/marks/teams/2?later_than=20
-get '/v1/marks/teams/:team_id' do
-end
+  # remove a member from a team
+  delete '/teams/:team_id/members/:user_id' do
+  end
 
-# get the list of marks shared by a user to a team
-# arguments:
-#     later_than - integer [OPTIONAL]
-# GET /v1/marks/teams/2/users/1?later_than=20
-get '/v1/marks/teams/:team_id/users/:user_id' do
-end
+  # Marks APIs
+  # get the list of marks for a team, grouped by user
+  # prepared for paperboy
+  # arguments:
+  #     later_than - integer [OPTIONAL]
+  # GET /v1/marks/teams/2?later_than=20
+  get '/marks/teams/:team_id' do
+  end
 
-# get a list of marks, by some filters
-# arguments:
-#     filter arguments, like channel, user_id, team_id
-#     later_than - integer [OPTIONAL]
-# GET /v1/marks?later_than=20&channel=1
-get '/v1/marks' do
-end
+  # get the list of marks shared by a user to a team
+  # arguments:
+  #     later_than - integer [OPTIONAL]
+  # GET /v1/marks/teams/2/users/1?later_than=20
+  get '/marks/teams/:team_id/users/:user_id' do
+  end
 
-# share a new mark
-post '/v1/marks' do
-end
+  # get a list of marks, by some filters
+  # arguments:
+  #     filter arguments, like channel, user_id, team_id
+  #     later_than - integer [OPTIONAL]
+  # GET /v1/marks?later_than=20&channel=1
+  get '/marks' do
+  end
 
-# Invitation APIs
-# get a bunch of invitation codes
-# arguments
-#     valid - true / false [OPTIONAL, default to true]
-#     num - integer [OPTIONAL, default to 1]
-# GET /v1/invitation-codes?num=5&valid=true
-get '/v1/invitation-codes' do
-end
+  # share a new mark
+  post '/marks' do
+  end
 
-# get the info for a specific code
-# GET /v1/invitation-codes/1
-get '/v1/invitation-codes/:id' do
-end
+  # Invitation APIs
+  # get a bunch of invitation codes
+  # arguments
+  #     valid - true / false [OPTIONAL, default to true]
+  #     num - integer [OPTIONAL, default to 1]
+  # GET /v1/invitation-codes?num=5&valid=true
+  get '/invitation-codes' do
+  end
 
-# create a new code
-post '/v1/invitation-codes' do
-end
+  # get the info for a specific code
+  # GET /v1/invitation-codes/1
+  get '/invitation-codes/:id' do
+  end
 
-# update the code info
-put '/v1/invitation-codes/:id' do
-end
+  # create a new code
+  post '/invitation-codes' do
+  end
 
-# delete the code info
-delete '/v1/invitation-codes/:id' do
+  # update the code info
+  put '/invitation-codes/:id' do
+  end
+
+  # delete the code info
+  delete '/invitation-codes/:id' do
+  end
 end
